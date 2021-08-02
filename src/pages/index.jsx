49b6398434280262e7fb3dc28bf9abc003964b99
John Carlo Cunanan <codingjohn@outlook.com>
2021-08-02 12:54:25 +0800

    refactor: remove unused variables, components and stylings

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -66,22 +66,7 @@ function Hero() {
           We are an operative co-founder of IT-companies with international growth ambitions. Our
           primary investment resources are time and knowledge to solve common biz and tech problems.
         </h4>
-        {/* <div className={styles.buttons}>
-          <Link
-            className={classnames(
-              'button button--outline button--lg',
-              styles.button,
-              styles.blueBtn
-            )}
-            to={useBaseUrl('docs/about/what-is-clutch')}
-          >
-            Learn More
-          </Link>
-        </div> */}
       </div>
-      {/* <div style={{ width: '20%', margin: '0 5% 5% 5%' }}>
-        { <img src={useBaseUrl('img/microsite/home.svg')} alt="home icon" /> }
-      </div> */}
     </header>
   );
 }
@@ -134,52 +119,6 @@ function Features() {
   );
 }
 
-// function Demo({ config }) {
-//   return (
-//     <div className={styles.darkBackground}>
-//       <section className={classnames('text--center', styles.section, styles.demoSection)}>
-//         <div className={classnames('container', styles.container, styles.demoContainer)}>
-//           {config.lines.map((line, idx) => (
-//             <h1 key={idx} className={classnames('hero__title', styles.sectionHeadingDark)}>
-//               {line}
-//             </h1>
-//           ))}
-//           <div style={{ display: 'flex', justifyContent: 'center' }}>
-//             <Link
-//               className={classnames(
-//                 'button button--outline button--lg',
-//                 styles.button,
-//                 styles.greenBtn,
-//                 styles.demoBtn
-//               )}
-//               to={useBaseUrl(config.cta.link)}
-//             >
-//               {config.cta.text}
-//             </Link>
-//           </div>
-//         </div>
-//       </section>
-//     </div>
-//   );
-// }
-
-// function Consolidation({ config }) {
-//   return (
-//     <section className={classnames(styles.section, styles.consolidation, styles.darkBackground)}>
-//       <div className={classnames('container', styles.container)}>
-//         <img src={useBaseUrl('img/microsite/consolidation.gif')} />
-//       </div>
-//       <div className={classnames('container', styles.container, styles.textContainer)}>
-//         <div>
-//           {config.snippets.map((snippet, idx) => (
-//             <p key={idx}>{snippet}</p>
-//           ))}
-//         </div>
-//       </div>
-//     </section>
-//   );
-// }
-
 function Slope() {
   return (
     <div className={styles.slopeContainer}>
@@ -197,8 +136,6 @@ export default function Home() {
       <Slope />
       <main>
         <Features config={sections.features} />
-        {/* <Demo config={sections.demo} />
-        <Consolidation config={sections.consolidation} /> */}
       </main>
     </Layout>
   );
