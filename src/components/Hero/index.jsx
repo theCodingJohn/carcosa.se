0dd35a253493c334f4c4c5907f464ff7dbf91abb
John Carlo Cunanan <codingjohn@outlook.com>
2021-08-04 13:23:59 +0800

    feat: add animation on hero texts using gsap

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import classnames from 'classnames';
+import gsap from 'gsap';
 
 import styles from './hero.module.css';
 
 const Hero = () => {
+  const heroTextWrapperRef = useRef(null);
+
+  useEffect(() => {
+    gsap.from(heroTextWrapperRef.current, { y: '3rem', opacity: 0 });
+
+  }, [heroTextWrapperRef]);
+
   return (
     <header className={classnames('hero hero--primary', styles.heroSection)}>
-      <div className={classnames('container', styles.container, styles.heroContainer)}>
+      <div ref={heroTextWrapperRef} className={classnames('container', styles.container, styles.heroContainer)}>
         <h1 className={classnames(styles.heroTitle)}>Venture Time</h1>
         <h4 className={classnames(styles.heroDescription)}>
 				We are an operative co-founder of IT-companies with international growth ambitions. Our
