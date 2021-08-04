234031e6606c802e66f10726a0f04bb2d313b26f
John Carlo Cunanan <codingjohn@outlook.com>
2021-08-04 12:39:58 +0800

    fix: navbar link hover state color

--- src/theme/Navbar/NavItem.jsx
+++ src/theme/Navbar/NavItem.jsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useState, useEffect }from 'react';
 import clsx from 'clsx';
 
+import styles from './styles.module.css';
 import NavLink from './NavLink';
 
 const NavItem = ({ items, position = 'right', className, ...props }) => {
+  const [pathName, setPathName] = useState(null);
+
+  useEffect(() => {
+    setPathName(window.location.pathname);
+  }, []);
+
   const navLinkClassNames = (extraClassName, isDropdownItem = false) =>
-    clsx(
+    clsx(pathName !== '/' && styles.navbarItem,
       {
         'navbar__item navbar__link': !isDropdownItem,
         'dropdown__link': isDropdownItem,
